80f3136cd613c4f212c5cf11daf09793995dd153
Alvaro Beltran Caceres <n01511461@humbermail.ca>
2023-02-17 16:01:58 -0500

    fixed subfolders in Models folder

--- FinalProject/Controllers/AccountController.cs
+++ FinalProject/Controllers/AccountController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProject.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Details() // Account acct
+        {
+            // create dummy account data
+            Account acct1 = new Account();
+            acct1.AcctId = 1;
+            acct1.Email = "tuyi.chen@humber.ca";
+            acct1.Password = "123456";
+            acct1.Phone = 6476471770;
+            acct1.Name = "Tuyi Chen";
+
+            return View(acct1);
         }
     }
 }
